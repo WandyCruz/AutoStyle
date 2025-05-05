9fac710e62bc4d0394d0277f7bce620f0bc1eee2
WandyCruz <wandy.cruz.dev@gmail.com>
2025-05-05 17:26:13 -0400

    Create javascript.js
    menu

---
+++ javascript.js
@@ -0,0 +1,21 @@
+onst menuToggle = document.getElementById('menuToggle');
+const mobileMenu = document.getElementById('mobileMenu');
+const closeMenu = document.getElementById('closeMenu');
+
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.remove('-translate-x-full');
+  document.body.classList.add('overflow-hidden');
+});
+
+closeMenu.addEventListener('click', () => {
+  mobileMenu.classList.add('-translate-x-full');
+  document.body.classList.remove('overflow-hidden');
+});
+
+// Cierra menú si se hace clic en un enlace
+document.querySelectorAll('#mobileMenu a').forEach(link => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.add('-translate-x-full');
+    document.body.classList.remove('overflow-hidden');
+  });
+});
